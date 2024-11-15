d3762468e85cc5cf3afa65341a0d7649239b5f02
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2024-11-15 22:30:37 +0900

    初回インストール不具合修正

--- EasyReforge/Setup.bat
+++ EasyReforge/Setup.bat
@@ -2,16 +2,14 @@
 chcp 65001 > NUL
 setlocal enabledelayedexpansion
 
-if exist %~dp0..\stable-diffusion-webui-reForge\ (
-	call %~dp0Reforge\Reforge.bat
-	if !ERRORLEVEL! neq 0 ( exit /b 1 )
+call %~dp0Reforge\Reforge.bat
+if !ERRORLEVEL! neq 0 ( exit /b 1 )
 
-	call %~dp0Reforge\ReforgeExtension.bat
-	if !ERRORLEVEL! neq 0 ( exit /b 1 )
+call %~dp0Reforge\ReforgeExtension.bat
+if !ERRORLEVEL! neq 0 ( exit /b 1 )
 
-	call %~dp0Reforge\ReforgeLink.bat
-	if !ERRORLEVEL! neq 0 ( exit /b 1 )
-)
+call %~dp0Reforge\ReforgeLink.bat
+if !ERRORLEVEL! neq 0 ( exit /b 1 )
 
 if exist %~dp0..\Model\Stable-diffusion\NoobE\ (
 	call %~dp0..\Download\NoobAiEpsilonPred_Minimum.bat
