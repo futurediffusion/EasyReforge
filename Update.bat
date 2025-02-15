141b271b7f1d3dfd66792c1719206dd6775ad810
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2025-02-16 03:41:04 +0900

    更新時に状態をリセット

--- Update.bat
+++ Update.bat
@@ -7,6 +7,8 @@ if %ERRORLEVEL% neq 0 ( exit /b 1 )
 pushd %~dp0EasyTools
 echo.
 echo https://github.com/Zuntan03/EasyTools
+echo git -C EasyTools switch -f main --quiet
+git -C EasyTools switch -f main --quiet
 echo git -C EasyTools pull
 git pull
 if %ERRORLEVEL% neq 0 ( pause & popd & exit /b 1 )
@@ -15,6 +17,8 @@ popd
 pushd %~dp0
 echo.
 echo https://github.com/Zuntan03/EasyReforge
+echo git -C EasyReforge switch -f main --quiet
+git switch -f main --quiet
 echo git -C EasyReforge pull
 git pull
 if %ERRORLEVEL% neq 0 ( pause & popd & exit /b 1 )
